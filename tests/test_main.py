c41cf65349847bed717783b33163a1d285868b74
dimaMukan <ds.mukan@gmail.com>
2023-03-30 10:59:09 +0200

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,15 @@
 from main import sort_area,sort_population
 import pytest
 
-def test_sort_area():
-    data = [('a', '-3'), ('b', '1'), ('c', '-2')]
-    assert sort_area(data) == [('b', '1'), ('c', '-2'), ('a', '-3')]
+# def test_sort_area():
+#     data = [('a', '-3'), ('b', '1'), ('c', '-2')]
+#     assert sort_area(data) == [('b', '1'), ('c', '-2'), ('a', '-3')]
+
 
 def test_sort_population():
     data = [('a', '1','1'), ('b', '1','100'), ('c', '1','20')]
     assert sort_population(data) == [('b', '1','100'), ('c', '1','20'), ('a', '1','1')]
+
 
 # @pytest.mark.parametrize("data, res", [
 #     (
@@ -18,5 +20,11 @@ def test_sort_population():
 # def test_sort_area(data, res):
 #     assert sort_area(data) == res
 
+@pytest.fixture
+def data():
+    return [('a', '3'), ('b', '1')]
 
+
+def test_sort_area(data):
+    assert sort_area(data) == [('a', '3'),('b', '1')]
 
