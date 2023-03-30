4ee5b6b735755c4b1cd9add94455f03471ff142e
dimaMukan <ds.mukan@gmail.com>
2023-03-30 10:35:23 +0200

    tetst

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,18 @@
 from main import sort_area,sort_population
-
-def test_sort_area():
-    pass
+import pytest
 
 def test_sort_population():
-    pass
+    data = [('a', '1','1'), ('b', '1','100'), ('c', '1','20')]
+    assert sort_population(data) == [('b', '1','100'), ('c', '1','20'), ('a', '1','1')]
+
+@pytest.mark.parametrize("data, res", [
+    (
+            [('a', '3'), ('b', '1'), ('c', '2')],
+            [('a', '3'), ('c', '2'), ('b', '1')]
+    ),
+])
+def test_sort_area(data, res):
+    assert sort_area(data) == res
+
+
+
