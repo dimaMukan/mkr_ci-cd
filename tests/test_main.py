0e1689485cbd591d1fd608cfe7b6192e96f71ad4
dimaMukan <ds.mukan@gmail.com>
2023-03-30 11:06:01 +0200

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 from main import sort_area,sort_population
 import pytest
 
-# def test_sort_area():
-#     data = [('a', '-3'), ('b', '1'), ('c', '-2')]
-#     assert sort_area(data) == [('b', '1'), ('c', '-2'), ('a', '-3')]
+def test_sort_area():
+    data = [('a', '-3'), ('b', '1'), ('c', '-2')]
+    assert sort_area(data) == [('b', '1'), ('c', '-2'), ('a', '-3')]
 
 
 def test_sort_population():
@@ -11,14 +11,14 @@ def test_sort_population():
     assert sort_population(data) == [('b', '1','100'), ('c', '1','20'), ('a', '1','1')]
 
 
-# @pytest.mark.parametrize("data, res", [
-#     (
-#             [('a', '3'), ('b', '1'), ('c', '2')],
-#             [('a', '3'), ('c', '2'), ('b', '1')]
-#     ),
-# ])
-# def test_sort_area(data, res):
-#     assert sort_area(data) == res
+@pytest.mark.parametrize("data, res", [
+    (
+            [('a', '3'), ('b', '1'), ('c', '2')],
+            [('a', '3'), ('c', '2'), ('b', '1')]
+    ),
+])
+def test_sort_area(data, res):
+    assert sort_area(data) == res
 
 @pytest.fixture
 def data():
