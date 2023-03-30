b892e69ea8832e41811c582ec62aae86cad3d4ce
dimaMukan <ds.mukan@gmail.com>
2023-03-30 09:59:11 +0200

    first def

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-with open("res.txt","w") as res_file:
-    with open("text.txt","r") as info_file:
-        for i in info_file:
-            print(i)
+import re
+
+def read():
+    a:list = []
+    with open("res.txt","w") as res_file:
+        with open("text.txt","r") as info_file:
+            for i in info_file:
+                s = re.sub(r'[\n]', '', i)
+                a.append(s)
+        data = [line.strip().split(',') for line in a]
+    return data
+
+print(read())
